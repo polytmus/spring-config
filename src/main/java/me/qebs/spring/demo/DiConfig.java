473a3bd41e8ebf5154a79b52fbc7c0c66ca3c93a
xuyao <xuyao12580@126.com>
2018-04-18 00:01:40 +0800

    first add

---
+++ src/main/java/me/qebs/spring/demo/DiConfig.java
@@ -0,0 +1,10 @@
+package me.qebs.spring.demo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("me.qebs.spring.demo")
+public class DiConfig {
+
+}
---
+++ src/main/java/me/qebs/spring/demo/UseFunctionService.java
@@ -0,0 +1,17 @@
+package me.qebs.spring.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UseFunctionService {
+
+
+    @Autowired
+    private FunctionService functionService;
+
+    public String sayHello(String word) {
+        return functionService.sayHello(word);
+    }
+}
+
