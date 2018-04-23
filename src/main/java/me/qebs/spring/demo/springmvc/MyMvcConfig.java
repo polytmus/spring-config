ade660e25e8adb2017895d25028f850a658ab807
xuyao <xuyao12580@126.com>
2018-04-23 22:24:52 +0800

    '2018-04-23'

---
+++ src/main/java/me/qebs/spring/demo/springmvc/MyMvcConfig.java
@@ -0,0 +1,23 @@
+package me.qebs.spring.demo.springmvc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("me.qebs.spring.demo.springmvc")
+public class MyMvcConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/classes/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+}
---
+++ src/main/resources/views/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xuyao
+  Date: 2018/4/23
+  Time: 21:53
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome to SpringMVC</title>
+</head>
+<body>
+    <pre>
+        Welcome to SpringMVC world.
+    </pre>
+</body>
+</html>
